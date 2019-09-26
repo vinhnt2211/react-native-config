8122ad844f325d48a966237769e8d607e9f9d4d0
Vinh Nguyen <vinh.nguyen@enouvo.com>
2019-09-26 12:00:29 +0700

    abced

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//Log12312312312
 // Bridge to:
 // Android: buildConfigField vars set in build.gradle, and exported via ReactConfig
 // iOS: config vars set in xcconfig and exposed via ReactNativeConfig.m
